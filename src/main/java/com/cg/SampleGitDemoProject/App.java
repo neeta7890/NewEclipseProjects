ee860ae963161b9eb2a6db112e82ed9718eb8dc9
nmanakaw <nmanakaw@INLEN8520027593.corp.capgemini.com>
2022-09-29 16:19:18 +0530

    this is first eclipse project to be pushed

---
+++ src/main/java/com/cg/SampleGitDemoProject/App.java
@@ -0,0 +1,13 @@
+package com.cg.SampleGitDemoProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
